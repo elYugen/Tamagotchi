ffdc3821574cfccbf2b6cc82b958c2564bb9d240
Léo Leroy <zsvm76m27x4gec@MacBookPro.lan>
2024-11-17 00:36:59 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -9,15 +9,15 @@ export default defineConfig({
         name: 'WatashiNoKodomo',
         short_name: 'Kodomo',
         description: 'Un jeu Tamagotchi en PWA',
-        theme_color: '#ffffff',
+        theme_color: '#1b1e26',
         icons: [
           {
-            src: '/icon-192x192.png',
+            src: 'https://imgs.search.brave.com/E9XEfQ7elR0i8OZjpZlC8K2BuL78YARn-Yq0YQ07z0I/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4w/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvZ2Vlay16b25l/LWljb25zLXJvdW5k/ZWQvMTEwL1RhbWFn/b3RjaGktNTEyLnBu/Zw',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/icon-512x512.png',
+            src: 'https://imgs.search.brave.com/E9XEfQ7elR0i8OZjpZlC8K2BuL78YARn-Yq0YQ07z0I/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4w/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvZ2Vlay16b25l/LWljb25zLXJvdW5k/ZWQvMTEwL1RhbWFn/b3RjaGktNTEyLnBu/Zw',
             sizes: '512x512',
             type: 'image/png'
           }
